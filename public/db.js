ff7c80a8d2c620cbc34166c362749b8f35a00c0c
Rau0622 <85261804+Rau0622@users.noreply.github.com>
2024-11-04 16:49:42 -0300

    Update db.js

--- public/db.js
+++ public/db.js
@@ -1,29 +1,13 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// Configuração da conexão com o banco de dados MySQL
-const db = mysql.createConnection({
-    host: '127.0.0.1',        // ou o IP do seu servidor MySQL
-    user: 'root',             // seu usuário MySQL
-    password: 'Rau_060203',    // sua senha MySQL
-    database: 'Projeto'        // nome do banco de dados que contém a tabela Tarefas
-});
-
-// Conecta ao banco de dados e realiza uma consulta de teste para verificar a conexão
-db.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-        return;
-    }
-    console.log('Conectado ao banco de dados MySQL!');
-
-    // Consulta de teste para verificar a comunicação com o banco de dados
-    db.query('SELECT 1 + 1 AS resultado', (err, results) => {
-        if (err) {
-            console.error('Erro ao executar a consulta de teste:', err);
-        } else {
-            console.log('Consulta de teste bem-sucedida. Resultado:', results[0].resultado);
-        }
-    });
+const db = mysql.createPool({
+    host: 'seu_host',        // IP ou nome do servidor
+    user: 'seu_usuario',     // seu usuário MySQL
+    password: 'sua_senha',   // sua senha MySQL
+    database: 'Projeto',     // nome do banco de dados
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
