465d23f177e75c6ba34bd135941b2f42f473f226
Rau0622 <85261804+Rau0622@users.noreply.github.com>
2024-11-05 17:47:27 -0300

    Update tarefas.js

--- functions/tarefas.js
+++ functions/tarefas.js
@@ -15,7 +15,7 @@ pool.query = util.promisify(pool.query);
 exports.handler = async (event) => {
     try {
         if (event.httpMethod === 'GET') {
-            const tarefas = await pool.query('SELECT * FROM tarefas');
+            const tarefas = await pool.query('SELECT * FROM Tarefas');
             return {
                 statusCode: 200,
                 body: JSON.stringify(tarefas),
@@ -23,7 +23,7 @@ exports.handler = async (event) => {
             };
         } else if (event.httpMethod === 'POST') {
             const { nome, custo, data_limite } = JSON.parse(event.body);
-            await pool.query('INSERT INTO tarefas (nome, custo, data_limite) VALUES (?, ?, ?)', [nome, custo, data_limite]);
+            await pool.query('INSERT INTO Tarefas (nome, custo, data_limite) VALUES (?, ?, ?)', [nome, custo, data_limite]);
             return {
                 statusCode: 201,
                 body: JSON.stringify({ message: 'Tarefa adicionada com sucesso!' }),
@@ -31,7 +31,7 @@ exports.handler = async (event) => {
             };
         } else if (event.httpMethod === 'PUT') {
             const { id, nome, custo, data_limite } = JSON.parse(event.body);
-            await pool.query('UPDATE tarefas SET nome = ?, custo = ?, data_limite = ? WHERE id = ?', [nome, custo, data_limite, id]);
+            await pool.query('UPDATE Tarefas SET nome = ?, custo = ?, data_limite = ? WHERE id = ?', [nome, custo, data_limite, id]);
             return {
                 statusCode: 200,
                 body: JSON.stringify({ message: 'Tarefa atualizada com sucesso!' }),
@@ -39,7 +39,7 @@ exports.handler = async (event) => {
             };
         } else if (event.httpMethod === 'DELETE') {
             const id = event.path.split('/').pop();
-            await pool.query('DELETE FROM tarefas WHERE id = ?', [id]);
+            await pool.query('DELETE FROM Tarefas WHERE id = ?', [id]);
             return {
                 statusCode: 204,
                 body: null
