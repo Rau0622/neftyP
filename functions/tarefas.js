08cdbcaebcb96affbfebb6bc98454a79d832ee54
Rau0622 <85261804+Rau0622@users.noreply.github.com>
2024-11-06 16:19:48 -0300

    Update tarefas.js

--- functions/tarefas.js
+++ functions/tarefas.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 const util = require('util');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST, // Host do banco de dados
-    user: process.env.DB_USER, // Usuário do banco de dados
-    password: process.env.DB_PASSWORD, // Senha do banco de dados
-    database: process.env.DB_NAME, // Nome do banco de dados
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     connectTimeout: 10000
 });
 
@@ -40,16 +40,31 @@ exports.handler = async (event) => {
                 headers: { 'Content-Type': 'application/json' }
             };
         } else if (event.httpMethod === 'PUT') {
-            const { id, nome, custo, data_limite } = JSON.parse(event.body);
-            await pool.query(
-                'UPDATE Tarefas SET nome = ?, custo = ?, data_limite = ? WHERE id = ?', 
-                [nome, custo, data_limite, id]
-            );
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ message: 'Tarefa atualizada com sucesso!' }),
-                headers: { 'Content-Type': 'application/json' }
-            };
+            const { id, nome, custo, data_limite, ordem } = JSON.parse(event.body);
+            
+            if (ordem !== undefined) {
+                // Atualiza apenas o campo ordem quando presente
+                await pool.query(
+                    'UPDATE Tarefas SET ordem = ? WHERE id = ?', 
+                    [ordem, id]
+                );
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Ordem da tarefa atualizada com sucesso!' }),
+                    headers: { 'Content-Type': 'application/json' }
+                };
+            } else {
+                // Atualiza outros campos caso ordem não esteja presente
+                await pool.query(
+                    'UPDATE Tarefas SET nome = ?, custo = ?, data_limite = ? WHERE id = ?', 
+                    [nome, custo, data_limite, id]
+                );
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Tarefa atualizada com sucesso!' }),
+                    headers: { 'Content-Type': 'application/json' }
+                };
+            }
         } else if (event.httpMethod === 'DELETE') {
             const id = event.path.split('/').pop();
             await pool.query('DELETE FROM Tarefas WHERE id = ?', [id]);
